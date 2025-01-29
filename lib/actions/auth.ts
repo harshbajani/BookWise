2aceac7732d02a3d8495540b3600ac73f71489ff
harshbajani <harshbajani7@gmail.com>
2025-01-29 20:26:39 +0530

    feat: enhance onboarding workflow with user activity tracking and email notifications

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import ratelimit from "../ratelimit";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -62,6 +64,11 @@ export const signUp = async (params: AuthCredentials) => {
       universityId,
       password: hasedPassword,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: { email, fullName },
     });
 
     await signInWithCredentials({ email, password });
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,6 +1,34 @@
-import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "./config";
+import { Client as QstashClient, resend } from "@upstash/qstash";
+import { Client as WorkflowClient } from "@upstash/workflow";
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashToken!,
   token: config.env.upstash.qstashToken!,
 });
+
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Acme <harshbajani7@gmail.com>",
+      to: [email],
+      subject: subject,
+      html: message,
+    },
+  });
+};
