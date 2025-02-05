0c7d91840262716b6c1aeb45bc1ca1ab8639d714
harshbajani <harshbajani7@gmail.com>
2025-02-05 19:58:56 +0530

    feat: add BookVideo component and sample book data; implement book details page

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/auth";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Page = () => {
   return (
